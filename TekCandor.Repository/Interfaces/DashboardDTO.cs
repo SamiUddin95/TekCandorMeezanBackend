d1ac01e3e69c48accea0c2212194ea397d1e6e55
WaleedRafique <159934027+WaleedRafique@users.noreply.github.com>
2026-04-06 13:02:59 +0500

    reports

--- TekCandor.Repository/Interfaces/DashboardDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TekCandor.Repository.Interfaces
-{
-    public class DashboardDTO
-    {
-        public string? Status { get; set; }
-        public int Cheques { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
